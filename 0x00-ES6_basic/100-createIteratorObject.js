c30d67e8c56237cdb0cd3fc9dbb80ce09b223cb0
minanadynarouz <mina.n.narouz@gmail.com>
2024-05-28 05:49:31 +0300

    Amending task 100

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,9 @@
-function createIteratorObject(report) {
+export default function createIteratorObject(report) {
   const emps = [];
   // eslint-disable-next-line
-    for (const dep of Object.keys(report.allEmployees)) {
+  for (const dep of Object.keys(report.allEmployees)) {
     // eslint-disable-next-line
-        for (const emp of report.allEmployees[dep]) {
+    for (const emp of report.allEmployees[dep]) {
       emps.push(emp);
     }
   }
