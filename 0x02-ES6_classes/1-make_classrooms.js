d93df6fe0cb283938bf8fde9cfcc5f66fdfd84b0
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-01 09:48:32 +0300

    task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from "./0-classroom";
+
+/**
+ * Creates an array of {@link ClassRoom}s with a specific size.
+ * @returns An array of {@link ClassRoom}s.
+ */
+
+export default function initializeRooms() {
+    [19, 20, 34].map((size) => new ClassRoom(size));
+}
