7e4fdd567203bdfa3b3bfb3eec80ed0e320f9c8f
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-04 22:18:08 +0300

    Amend style

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,14 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-  .then(() => {
-    console.log('Got a response from the API');
+    .then(() => {
+      console.log('Got a response from the API');
       return {
         status: 200,
         body: 'success'
-    };
-})
-  .catch(() => {
-    console.log('Got an response from the API');
-    return new Error();
-  })
+      };
+    })
+    .catch(() => {
+      console.log('Got an response from the API');
+      return new Error();
+    })
 }
