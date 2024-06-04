8f4ab87054ee26a68da6160536d4a9c7609dd002
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-05 00:01:25 +0300

    Task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+import Currency from './3-currency';
+
+/**
+ * Represents a Pricing .
+ */
+
+export default class Pricing extends Currency {
+  constructor(amount, currency) {
+    super(code, currency);
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getters
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  // Setters
+
+  set amount(val) {
+    this._amount = val;
+  }
+
+  set currency(val) {
+    this._currency = val;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency} (${this._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return this._amount * conversionRate;
+  }
+}
