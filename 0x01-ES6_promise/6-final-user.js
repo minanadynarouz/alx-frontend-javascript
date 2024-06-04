8cd40a2b3d8e8815efbea367bccaf091e0dcb72b
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-04 23:21:38 +0300

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,18 +6,12 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   return Promise.allSettled(promises)
     .then((results) => {
-      results.forEach((result) => {
+      const resultArr = results.map((result) => {
         if (result.status === 'fulfilled') {
-          console.log({
-            status: result.status,
-            value: result.value,
-          });
-        } else {
-          console.log({
-            status: result.status,
-            reason: result.reason,
-          });
+          return { status: result.status, value: result.value };
         }
+        return { status: result.status, reason: result.reason };
       });
+      return resultArr;
     });
 }
