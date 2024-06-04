dffdb9fb52dfb1d5db7fa9e040540e537d1c255c
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-04 23:13:17 +0300

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,3 +1,18 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+export default function handleProfileSignup(firstName, LastName, fileName) {
+  const promises = [signUpUser(firstName, LastName), uploadPhoto(fileName)];
+
+  return Promise.allSettled(promises)
+    .then((results) => {
+      results.forEach((result) => {
+        [
+          {
+            status: result.status,
+            value: result.value
+          }
+        ]
+      })
+    })
+}
