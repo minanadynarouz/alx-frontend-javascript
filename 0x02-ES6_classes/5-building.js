0e7d36c10a322156a3051bbdeb0022f02e8d8426
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-05 06:54:29 +0300

    Adding task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+export default class BUilding {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+
+}
+
