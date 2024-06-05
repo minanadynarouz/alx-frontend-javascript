1d2da15a947a3f3f9a0ff9f24070c9be319bcc7e
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-05 07:29:07 +0300

    task 100

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,1 +1,13 @@
 import Car from "./10-car";
+
+export default class EVCar extends Car {
+  constructor(range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  cloneCar() {
+    return new Car();
+  }
+}
