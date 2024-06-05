3bde0f09f261116245e3f354cf020c5e715db3fe
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-05 06:56:49 +0300

    Amending task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -4,7 +4,7 @@ import Currency from './3-currency';
  * Represents a Pricing .
  */
 
-export default class Pricing extends Currency {
+export default class Pricing {
   constructor(amount, currency) {
     super(code, currency);
     this._amount = amount;
@@ -31,7 +31,7 @@ export default class Pricing extends Currency {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${Currency._name} (${Currency._code})`;
+    return `${this._amount} ${new Currency(this._currency.code, this._currency.name).displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
