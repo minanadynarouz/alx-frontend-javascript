4b0cf97fed901d5fc276c97ef79d0b7e42a74333
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-05 05:53:17 +0300

    Task 2

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(studentList) {
+  if (!(Array.isArray(studentList))) {
+    return [];
+  }
+
+  const ids = studentList.map((student) => student.id);
+  return ids;
+}
