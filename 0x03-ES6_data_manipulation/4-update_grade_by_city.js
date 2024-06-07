ead19301409ad93e0b6128c5b42bf835901a1fe6
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-07 14:25:33 +0300

    Task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
+  return getListStudents
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
