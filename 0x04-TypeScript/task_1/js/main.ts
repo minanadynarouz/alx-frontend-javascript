a1ed0a6b8e6682f6de085de71e2963e42d4ec8b8
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-12 01:50:23 +0300

    Task 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -13,3 +13,13 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const firstLetter = firstName.charAt(0).toUpperCase();
+  const fullName = `${firstLetter}. ${lastName}`;
+  return fullName;
+};
