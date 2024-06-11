cd720ea6b4b01a02fc64e504634de243fcb4db4d
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-12 02:43:57 +0300

    Adding Task 7

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -43,3 +43,15 @@ function executeWork(employee: Director | Teacher): void {
     console.log(employee.workTeacherTasks);
   }
 }
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects) {
+  if (todayClass === 'Math') {
+    return `Teaching Math`;
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  } else {
+    return 'No Subject';
+  }
+}
