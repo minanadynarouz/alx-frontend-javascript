73e5ae24d17dccddae5bd7adef00c7437f50022d
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-12 19:25:04 +0300

    Task9

---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,25 @@
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingReact?: number; // Updated using declaration merging
+  }
+
+  export class React extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for React';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingReact) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return 'No available teacher'
+      }
+    }
+  }
+}
+
+
+
